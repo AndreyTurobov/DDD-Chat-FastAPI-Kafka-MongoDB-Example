168a27e11c2725e641bb2292e8454915dd51b3ce
andrei_turobov <forgunn@yandex.ru>
2024-05-26 15:46:00 +0500

    added websocket connect condition.

--- app/application/api/messages/websockets/messages.py
+++ app/application/api/messages/websockets/messages.py
@@ -8,7 +8,10 @@ from starlette.websockets import (
 from punq import Container
 
 from infra.websockets.managers import BaseConnectionManager
+from logic.exceptions.messages import ChatNotFoundException
 from logic.init import init_container
+from logic.mediator.base import Mediator
+from logic.queries.messages import GetChatDetailQuery
 
 router = APIRouter(tags=["chats"])
 
@@ -20,6 +23,15 @@ async def websocket_endpoint(
     container: Container = Depends(init_container),
 ) -> None:
     connection_manager: BaseConnectionManager = container.resolve(BaseConnectionManager)
+    mediator: Mediator = container.resolve(Mediator)
+
+    try:
+        await mediator.handle_query(GetChatDetailQuery(chat_oid=chat_oid))
+    except ChatNotFoundException as error:
+        await websocket.accept()
+        await websocket.send_json(data={"error": error.message})
+        await websocket.close()
+
     await connection_manager.accept_connection(websocket=websocket, key=chat_oid)
 
     await websocket.send_text("You are now connected!")
